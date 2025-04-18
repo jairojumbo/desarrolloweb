5d86792a4e9c3a7b353965465022d82166a17c9e
Jairo Jumbo <jairojumbo@gmail.com>
2025-04-18 14:10:10 -0500

    dom2

---
+++ modulo1/ejemplos/DOM-2/script.js
@@ -0,0 +1,32 @@
+// Acceso al DOM usando ID
+const title = document.getElementById('main-title');
+title.textContent = 'Hola Mundo, JavaScript!';
+
+// Acceso al DOM usando clase
+const paragraphs = document.getElementsByClassName('text');
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = 'blue';
+}
+
+// Manejo de eventos
+const button = document.getElementById('change-text');
+button.addEventListener('click', function() {
+    title.textContent = 'Titulo Cambiado!';
+});
+
+// Mostrar la lista de nombres
+const showNamesButton = document.getElementById('show-names');
+const nameList = document.getElementById('name-list');
+const names = ['Juan', 'Maria', 'Carlos', 'Ana', 'Luis'];
+
+showNamesButton.addEventListener('click', function() {
+    // Limpiar la lista antes de agregar los nuevos nombres
+    nameList.innerHTML = '';
+    
+    // Crear una lista de elementos <li> con los nombres
+    names.forEach(function(name) {
+        const listItem = document.createElement('li');
+        listItem.textContent = name;
+        nameList.appendChild(listItem);
+    });
+});
